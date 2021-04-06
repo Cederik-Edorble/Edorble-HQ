dc48329a3d4c1498deb6027e51e3d1391c3dff36
krishna rokhale <krishnarokhale@krishnas-Mac-mini.local>
2021-04-06 14:54:35 +0530

    checking in

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,7 @@ colors.edorble = edorble
 colors['edorble-yellow'] = edorbleYellow
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
