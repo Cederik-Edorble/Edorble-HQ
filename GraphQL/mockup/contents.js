29f028297d7521a04965e83291bad35fd5d5df5c
Yuriy Lunyov <lunyov.yu@gmail.com>
2021-08-06 09:57:33 +0300

    restored new folder

---
+++ GraphQL/mockup/contents.js
@@ -0,0 +1,14 @@
+const contents = [
+  {
+    id: 1,
+    screen: 1,
+    created: '1626778875835',
+    type: 'img',
+    url: 'https://google.com',
+    description: 'description',
+    title: 'very long string in title name',
+  }
+];
+module.exports = {
+  contents
+};
---
+++ GraphQL/mockup/region.js
@@ -0,0 +1,29 @@
+const regions = [
+  {
+    id: 1,
+    name: 'user_region_1',
+    map: 1,
+    created: 1626778875835
+  },
+  {
+    id: 2,
+    name: 'user_region_2',
+    map: 1,
+    created: 1626778875835
+  },
+  {
+    id: 3,
+    name: 'user_region_3',
+    map: 2,
+    created: 1626778875835
+  },
+  {
+    id: 4,
+    name: 'user_region_4',
+    map: 2,
+    created: 1626778875835
+  }
+];
+module.exports = {
+  regions
+};
